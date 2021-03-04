3f7f12a81965ddb21492dd2bb9c616589e00adc7
Joseph Ojoko <joseph.ojoko@cardinalstone.com>
2021-03-04 16:34:03 +0100

    add main entry file, use card component

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="center">
+    <card :name="name" :expiry="expiry" :cvv="cvv" :number="number" />
+  </div>
+</template>
+
+<script>
+import Card from "./Card.vue"
+export default {
+  components: { Card },
+  data() {
+    return {
+      name: "Cardholder Name",
+      expiry: "10/12",
+      cvv: "455",
+      number: "9784978497849784"
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+*, *::before, *::after
+  box-sizing border-box
+
+html, body
+  font 16px/1.2 BlinkMacSystemFont, -apple-system, "Segoe UI", Roboto, Helvetica, Arial, sans-serif
+  padding 10px
+
+.center
+    min-height 100vh
+    display flex
+    justify-content center
+    align-items center
+</style>
